5b246b91116ef25d40d60a0e8072cb31f258fb30
CDH1126 <donghuyn1126@gmail.com>
2026-04-14 09:45:39 +0900

    feat : 회원가입 양식을 위한 Dto 클래스 추가

---
+++ src/main/java/comso/Team5/GP/users/dto/SignupRequestDto.java
@@ -0,0 +1,25 @@
+package comso.Team5.GP.users.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class SignupRequestDto {
+
+    @NotBlank(message = "아이디를 입력해주세요.")
+    @Size(min = 5, max = 20, message = "아이디는 5~20자 사이여야 합니다.")
+    private String id;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    private String password;
+
+    @NotBlank(message = "이름(닉네임)을 입력해주세요.")
+    private String name;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+}
