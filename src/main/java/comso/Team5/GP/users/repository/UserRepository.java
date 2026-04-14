439f28059451d20dc83aeb63e8cf28cf113b7925
CDH1126 <donghuyn1126@gmail.com>
2026-04-14 13:42:38 +0900

    fix : UserRepository - findByLoginId의 필드 이름 변경

--- src/main/java/comso/Team5/GP/users/repository/UserRepository.java
+++ src/main/java/comso/Team5/GP/users/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
 
-    Optional<Users> findByLoginId(String loginId);
+    Optional<Users> findById(String id);
 
     ///  이메일 중복 체크
     boolean existsById(String id);
