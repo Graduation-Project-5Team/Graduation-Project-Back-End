71b8dfab936cfa1d42bde7db10b3567d1618bb59
CDH1126 <90539594+CDH1126@users.noreply.github.com>
2026-04-28 19:31:09 +0900

    refactor: 테이블 스펙 기준으로 엔티티 및 DTO 정합성 수정 (#8)

--- src/main/java/comso/Team5/GP/artworks/dto/request/ArtworkUpdateRequest.java
+++ src/main/java/comso/Team5/GP/artworks/dto/request/ArtworkUpdateRequest.java
@@ -2,15 +2,16 @@ package comso.Team5.GP.artworks.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
-public class ArtworkCreateRequest {
-    private Long userId;
-
-    private Long exhiId;
+public class ArtworkUpdateRequest {
 
     private String title;
 
     private String description;
+
+    // TODO images[] : 이미지 테이블 구현 완료 후 추가 예정
 }
---
+++ src/main/java/comso/Team5/GP/artworks/entity/ArtworkImages.java
@@ -0,0 +1,31 @@
+package comso.Team5.GP.artworks.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "artwork_images")
+public class ArtworkImages {
+
+    @Id
+    @Column(name = "artwork_image_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long artworkImageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artwork_id", referencedColumnName = "artwork_id")
+    private Artworks artwork;
+
+    @Column(name = "image_url", length = 255)
+    private String imageUrl;
+
+    @Column(name = "sort_order")
+    private int sortOrder;
+}
